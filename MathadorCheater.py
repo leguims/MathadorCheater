f96d1bfef76820cebfada2a712e620aa48d8be0d
leguims <leguims@free.fr>
2019-01-02 12:56:23 +0100

    Fix zero divide bug.

--- MathadorCheater.py
+++ MathadorCheater.py
@@ -2,17 +2,32 @@ from itertools import permutations, product, combinations#, combinations_with_re
 from operator import add, truediv, sub, mul # truediv = float div
 
 #Enonce
-numbers=[2, 5, 8, 11, 15]
-result=44
+level_unknown=[
+    {'result': 44, 'numbers':(2, 5, 8, 11, 15)},
+    {'result': 60, 'numbers':(2, 3, 7, 8, 15)},
+    {'result': 50, 'numbers':(3, 6, 8, 9, 14)},
+    {'result': 42, 'numbers':(3, 4, 5, 7, 9)},
+    ]
 
-##numbers=[2, 3, 7, 8, 15]
-##result=60
+level_1=[
+    {'result': 6, 'numbers':(1, 12, 9, 2, 2)},
+    {'result': 6, 'numbers':(4, 9, 3, 8, 2)},
+    {'result': 4, 'numbers':(6, 2, 12, 4, 5)},
+    {'result': 8, 'numbers':(8, 2, 2, 10, 8)},
+    {'result': 6, 'numbers':(1, 2, 9, 12, 2)},
+    {'result': 3, 'numbers':(1, 9, 2, 2, 12)},
+    ]
 
-##numbers=[3, 6, 8, 9, 14]
-##result=50
+level_2=[
+    {'result': 18, 'numbers':(9, 1, 2, 11, 4)},
+    {'result': 12, 'numbers':(9, 12, 2, 1, 2)},
+    {'result': 11, 'numbers':(3, 5, 7, 4, 1)},
+    {'result': 10, 'numbers':(7, 9, 6, 14, 1)},
+    {'result': 5, 'numbers':(2, 3, 4, 8, 9)},
+    {'result': 14, 'numbers':(9, 7, 9, 1, 2)},
+    ]
 
-##numbers=[3, 4, 5, 7, 9, ]
-##result=42
+to_solve=level_1[0]
 
 # Score:
 #   18 points : All operations are used
@@ -24,6 +39,8 @@ result=44
 
 
 #teatments
+numbers=to_solve['numbers']
+result=to_solve['result']
 operations=[add, truediv, sub, mul]
 operationsSigns={k.__name__:v for (k,v) in zip(operations, ['+',':','-','x'])}
 operationsScore={k.__name__:v for (k,v) in zip(operations, [ 1 , 3 , 2 , 1 ])}
@@ -50,76 +67,97 @@ for (n1, n2, n3, n4, n5) in loop_numbers:
         # Make it easier to print
         (ops1, ops2, ops3, ops4) = [operationsSigns[op.__name__] for op in (op1, op2, op3, op4)]
 
-        # Mathador !
-        if op4( op3( op2( op1(n1, n2), n3), n4), n5) == result:
-            solutionString = "{op1}{op2}{op3}{op4} : [ ( [ ({n1}{op1}{n2}) {op2}{n3} ] {op3}{n4}) {op4}{n5}] = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                op2 = ops2, n3 = n3,
-                op3 = ops3, n4 = n4,
-                op4 = ops4, n5 = n5,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1, op2, op3, op4)
-        if op4( op3(op1(n1, n2), op2(n3,n4)), n5) == result:
-            solutionString = "{op1}{op3}{op2}{op4} : [ ({n1}{op1}{n2}) {op3} ({n3}{op2}{n4}) ] {op4}{n5} = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                op2 = ops2, n3 = n3, n4 = n4,
-                op3 = ops3, 
-                op4 = ops4, n5 = n5,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1, op2, op3, op4)
-        if op4( op2( op1(n1, n2), n3), op3(n4, n5)) == result:
-            solutionString = "{op1}{op2}{op4}{op3} : [ ({n1}{op1}{n2}) {op2}{n3} ] {op4} ({n4}{op3}{n5}) = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                op2 = ops2, n3 = n3,
-                op3 = ops3, n4 = n4, n5 = n5,
-                op4 = ops4,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1, op2, op3, op4)
+        # Mathador or 4 operators !
+        try:
+            if op4( op3( op2( op1(n1, n2), n3), n4), n5) == result:
+                solutionString = "{op1}{op2}{op3}{op4} : [ ( [ ({n1}{op1}{n2}) {op2}{n3} ] {op3}{n4}) {op4}{n5}] = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    op2 = ops2, n3 = n3,
+                    op3 = ops3, n4 = n4,
+                    op4 = ops4, n5 = n5,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1, op2, op3, op4)
+        except ZeroDivisionError:
+            pass
+        try:
+            if op4( op3(op1(n1, n2), op2(n3,n4)), n5) == result:
+                solutionString = "{op1}{op3}{op2}{op4} : [ ({n1}{op1}{n2}) {op3} ({n3}{op2}{n4}) ] {op4}{n5} = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    op2 = ops2, n3 = n3, n4 = n4,
+                    op3 = ops3, 
+                    op4 = ops4, n5 = n5,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1, op2, op3, op4)
+        except ZeroDivisionError:
+            pass
+        try:
+            if op4( op2( op1(n1, n2), n3), op3(n4, n5)) == result:
+                solutionString = "{op1}{op2}{op4}{op3} : [ ({n1}{op1}{n2}) {op2}{n3} ] {op4} ({n4}{op3}{n5}) = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    op2 = ops2, n3 = n3,
+                    op3 = ops3, n4 = n4, n5 = n5,
+                    op4 = ops4,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1, op2, op3, op4)
+        except ZeroDivisionError:
+            pass
 
-        # Others
-        if op3( op2( op1(n1, n2), n3), n4) == result:
-            solutionString = "{op1}{op2}{op3} : [ ({n1}{op1}{n2}) {op2}{n3} ] {op3}{n4} = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                op2 = ops2, n3 = n3,
-                op3 = ops3, n4 = n4,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1, op2, op3)
-        if op3( op1(n1, n2), op2(n3, n4) ) == result:
-            solutionString = "{op1}{op3}{op2} : ({n1}{op1}{n2}) {op3} ({n3}{op2}{n4}) = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                op2 = ops2, n3 = n3,
-                op3 = ops3, n4 = n4,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1, op2, op3)
-        if op2( op1(n1, n2), n3) == result:
-            solutionString = "{op1}{op2} : ({n1}{op1}{n2}) {op2}{n3} = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                op2 = ops2, n3 = n3,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1, op2)
-        if op1(n1, n2) == result:
-            solutionString = "{op1} : ({n1}{op1}{n2}) = {result}".format(
-                op1 = ops1, n1 = n1, n2 = n2,
-                result = result)
-            #print(solutionString)
-            save_solution(solutions, solutionString, op1)
+        # Less than 4 operators
+        try:
+            if op3( op2( op1(n1, n2), n3), n4) == result:
+                solutionString = "{op1}{op2}{op3} : [ ({n1}{op1}{n2}) {op2}{n3} ] {op3}{n4} = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    op2 = ops2, n3 = n3,
+                    op3 = ops3, n4 = n4,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1, op2, op3)
+        except ZeroDivisionError:
+            pass
+        try:
+            if op3( op1(n1, n2), op2(n3, n4) ) == result:
+                solutionString = "{op1}{op3}{op2} : ({n1}{op1}{n2}) {op3} ({n3}{op2}{n4}) = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    op2 = ops2, n3 = n3,
+                    op3 = ops3, n4 = n4,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1, op2, op3)
+        except ZeroDivisionError:
+            pass
+        try:
+            if op2( op1(n1, n2), n3) == result:
+                solutionString = "{op1}{op2} : ({n1}{op1}{n2}) {op2}{n3} = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    op2 = ops2, n3 = n3,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1, op2)
+        except ZeroDivisionError:
+            pass
+        try:
+            if op1(n1, n2) == result:
+                solutionString = "{op1} : ({n1}{op1}{n2}) = {result}".format(
+                    op1 = ops1, n1 = n1, n2 = n2,
+                    result = result)
+                #print(solutionString)
+                save_solution(solutions, solutionString, op1)
+        except ZeroDivisionError:
+            pass
 
 print("Find {} with these numbers : {} and operations {}".format(int(result), [int(n) for n in numbers], operationsSigns.values()))
 print("{} possibilies to sort operations and numbers.".format(len(loop_numbers)*len(loop_operations)))
 print("Found {} solutions below.".format( sum([len(i) for i in solutions.values()]) ))
 print(15*4*"=")
 for (k,v) in solutions.items() :
-    print("Found {} solution{} with {} point{}{}".format(len(v),
+    print("Found {} solution{} with {} point{}:{}".format(len(v),
                                                  "s" if len(v)>1 else "",
                                                  k,
                                                  "s" if k>1 else "",
-                                                 ":" if len(v)>0 else "."))
+                                                 " ==> Mathador <==" if k==18 else ""))
     for s in v:
         print("{}{}".format(4*" ", s))
     print(15*"-=-.")
